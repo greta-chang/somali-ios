2627ace9d6c0ade3455ee35d1ab0d170c5196202
Greta Chang <greta.chang@epicgames.com>
2021-02-05 15:21:40 -0800

    initial commit

---
+++ somali-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  somali-ios
+//
+//  Created by greta.chang on 2/5/21.
+//  Copyright © 2021 greta.chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
